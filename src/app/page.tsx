747ebfe9ae2253e212c786a78a7ff7b0d6fcfaf6
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-04-28 00:21:27 -0600

    ⭐ -> PDF Extractor & read others files like .TXT and more

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
+'use client'
+
+import { FileType, isAcceptedFileType } from "@/services/files/file_type";
+import { pdfToText } from "@/services/files/pdfs/pdf_to_text";
+import { FormDataFile } from "@/utils/extract_form_data";
+import { useState } from "react";
+
 export default function Home() {
+  const [text, setText] = useState<string>()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const file = FormDataFile(e);
+
+    // Is file is not accepted, return 
+    if (!isAcceptedFileType(file)) return
+
+    // If file is a PDF, extract text
+    if (FileType(file) === 'PDF') {
+      const text_extracted = await pdfToText(file)
+      if (!text_extracted) return
+      setText(text_extracted);
+    }
+  }
+
   return (
     <main className="h-screen">
-      <h1>Home</h1>
+      <form action="" onSubmit={handleSubmit} className="flex flex-col gap-4 bg-red-50 max-w-sm mx-auto mt-32">
+        <input type="file" name="file" id="file" accept=".pdf, .doc, .docx, .txt" />
+        <button type="submit" className="rounded bg-black text-white p-2 mx-4">Send file</button>
+      </form>
+
+      <p className="max-w-[70ch] mt-20 mx-auto">
+        {text}
+      </p>
     </main>
   );
 }
---
+++ src/utils/extract_form_data.ts
@@ -0,0 +1,8 @@
+export function FormDataFile(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  const formData = new FormData();
+  const file = (e.target as HTMLFormElement).file.files[0];
+  formData.append('file', file);
+
+  return file;
+}
