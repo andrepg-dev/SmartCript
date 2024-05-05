ed6ee4f1daf0754c3a901728902e7b339a551a83
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-05-05 07:46:20 -0600

    🎨 -> Ajustando el avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,8 @@
-'use client'
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  const sendEmail = async () => {
-    const product: Product = {
-      id: 'smartcript-plan-mensual', // 'smartcript-plan-anual'
-      name: 'Carrito de supermercado inteligente',
-      images: ['https://i.imgur.com/EHyR2nP.png'],
-      price: 35,
-    }
-
-    const res = await fetch('/api/stripe', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(product)
-    })
-
-    const session = await res.json() as { url: string }
-    window.location.href = session.url;
-  }
+export default function HomePage() {
 
   return (
     <main className="h-screen w-full flex items-center justify-center">
-      <Button onClick={sendEmail}>Comprar servicio</Button>
+
     </main>
   )
 }
---
+++ src/components/project/navigation/profile-dropdown.tsx
@@ -0,0 +1,59 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { HomeIcon, LayoutDashboard, LogOut, Settings } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+
+interface ProfileDropdownProps {
+  avatar_url: string;
+  avatarBgColor: string;
+  fullName: string;
+  email: string;
+}
+
+export default function ProfileDropdown({ avatar_url, fullName, email, avatarBgColor }: ProfileDropdownProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant={'ghost'} size={'icon'}>
+          <Avatar className={cn('size-6', avatarBgColor)}>
+            <AvatarImage src={avatar_url} alt={`${fullName} profile image`} />
+            <AvatarFallback>SC</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel className="flex flex-col">
+          <span>{fullName}</span>
+          <span className="text-xs text-muted-foreground font-normal">{email}</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <HomeIcon size={16} className="mr-2" />
+          Home
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <LayoutDashboard size={16} className="mr-2" />
+          Dashboard
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Settings size={16} className="mr-2" />
+          Settings
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <LogOut size={16} className="mr-2" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+
+}
