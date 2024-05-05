55c978073f25cad00bff2861fc1ce69118115a1c
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-05-05 02:24:24 -0600

    ⚙️ -> Retorning file with type

--- src/utils/extract_form_data.ts
+++ src/utils/extract_form_data.ts
@@ -2,7 +2,7 @@ export function FormDataFile(e: React.FormEvent<HTMLFormElement>) {
   e.preventDefault();
 
   const formData = new FormData();
-  const file = (e.target as HTMLFormElement).file.files[0];
+  const file = (e.target as HTMLFormElement).file.files[0] as File;
   formData.append('file', file);
 
   return file;
