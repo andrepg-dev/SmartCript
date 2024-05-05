e140e294a07ffb2c895710a5d3abb7b82c7313e2
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-05-05 02:29:10 -0600

    ⚙️ -> Obtener los usuarios

---
+++ src/app/api/database/route.ts
@@ -0,0 +1,44 @@
+
+import { connection } from '@/utils/database'
+import { DBUsers } from '@/services/database/users'
+import { DBUser as IDBusers } from '@/interfaces/db-user'
+import { gen_random_uuid } from '@/services/database/gen_uuid';
+
+/**
+-- DROP TABLE IF EXISTS USERS;
+
+-- CREATE TABLE USERS(
+-- 	ID UUID NOT NULL PRIMARY KEY,
+-- 	FULLNAME VARCHAR(450) NOT NULL,
+-- 	EMAIL VARCHAR(255) NOT NULL UNIQUE,
+-- 	PASSWORD VARCHAR(255),
+-- 	SUSCRIPTION_ID VARCHAR(50),
+-- 	CREATED_AT DATE DEFAULT CURRENT_DATE
+-- );
+
+    SELECT * FROM USERS;
+
+-- SELECT gen_random_uuid() AS uuid 
+   */
+
+export async function GET(): Promise<Response> {
+  // Sino hay conexión a la base de datos retornar un error
+  if (!connection) return Response.json({ message: 'Database connection error' }, { status: 500 })
+
+  const uuid = await gen_random_uuid();
+
+  const newUser: IDBusers = {
+    id: uuid,
+    fullName: 'horik',
+    email: 'leo@gmail.com',
+    password: 'casaca',
+    suscriptionId: '1883_32348'
+  }
+
+  await new DBUsers().createUser(newUser);
+
+  // Obtener todos los usuarios
+  const users = await new DBUsers().getUsers();
+
+  return Response.json({ users }) // Verificar si el servidor está funcionando
+} 
