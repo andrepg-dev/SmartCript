d1a3b4a00b93865e56dc3deecdffd0116b548837
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-05-09 01:37:44 -0600

    📝 -> Testeando la creacion del usuario desde la API

--- src/app/api/database/route.ts
+++ src/app/api/database/route.ts
@@ -3,6 +3,7 @@ import { connection } from '@/utils/database'
 import { DBUsers } from '@/services/database/users'
 import { DBUser as IDBusers, UserSuscription } from '@/interfaces/db-user'
 import { gen_random_uuid } from '@/services/database/gen_uuid';
+import { gen_current_date } from '@/services/database/gen_current_date';
 
 /**
 -- DROP TABLE IF EXISTS USERS;
@@ -30,16 +31,26 @@ export async function GET(): Promise<Response> {
 
   const newUser: IDBusers = {
     id: uuid,
-    fullName: 'horik',
-    email: 'usersuscriptionPREMIUM@gmail.com',
+    fullName: 'Andre Sebastian Ponce Garcia',
+    email: 'buenas@gmail.com',
     avatar: 'https://avatar.png',
-    password: 'cortala farmi',
+    password: '$writting an strong password_',
     suscriptionId: UserSuscription.PREMIUM,
-    avatarColor: 'bg-blue-300'
+    avatarColor: 'bg-blue-300',
+    payment_date: await gen_current_date()
   }
 
-  await new DBUsers().createUser(newUser).catch(err => console.log('Ha ocurrido un error'));
- 
+  await new DBUsers().createUser(newUser).catch(err => console.log('usuario duplicado'));
+
+  // Esperar 3 segundos para que se cree el usuario
+  newUser.id = await gen_random_uuid();
+  newUser.email = 'otroemail@gmail.com'
+  newUser.payment_date = null;
+  newUser.suscriptionId = UserSuscription.FREE;
+  newUser.payment_date = '2021-10-10';
+
+  await new DBUsers().createUser(newUser).catch(err => console.log('usuario duplicado'));
+
   // Obtener todos los usuarios
   const users = await new DBUsers().getUsers();
 
