53f799aff97481a9274d1f3353213c0dbffab4d2
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-05-12 19:47:28 -0600

    🎨 -> New color added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        purple: '#B249F8',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
@@ -67,11 +68,29 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'text-gradient': {
+          '0%': { backgroundPosition: '200%' },
+          '50%': { backgroundPosition: '100% ' }
+        },
+        'login-background': {
+          '0%': { top: '0', left: '0' },
+          '100%': { top: '-20rem', left: '-15rem' },
+        },
+        'fade':{
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1'}
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'text-gradient': 'text-gradient 4s linear 0s infinite normal forwards running ',
+        'login-background': 'login-background 20s linear alternate infinite',
+        'fade': 'fade 2.5s ease-in-out'
       },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+      }
     },
   },
   plugins: [require('tailwindcss-animate')],
