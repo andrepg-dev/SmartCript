f24d33bec02d21441fdce33e0e2488791306a5fb
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-07-20 12:13:56 -0600

    🐛 Error de deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,5 @@
-import { AuthOptions } from 'next-auth';
+import { authOptions } from '@/utils/authOptions';
 import NextAuth from 'next-auth/next';
-import GoogleProvider from 'next-auth/providers/google';
-
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ]
-}
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
-
