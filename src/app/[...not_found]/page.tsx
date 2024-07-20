31cdf760f81e2ae35a5f08b27a65bd022b08a893
Andre <89555120+AndrePonce3322@users.noreply.github.com>
2024-07-20 11:56:46 -0600

    🎨 Nuevo diseño para las páginas 404

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -8,10 +8,18 @@ export default function NotFoundPage() {
 
       <IconLocationNotFound className="w-24 h-24 fill-primary" />
 
-      <p>No se pudo encontrar esta página.</p>
-      <Link href={'/'}>
-        <Button>Regresar al inicio</Button>
-      </Link>
+      <div className="flex flex-col text-center gap-2 mt-2">
+        <h2>¡Ups! ¡Página no encontrada!</h2>
+        <p className="text-muted-foreground">Parece que la página que estás buscando <br /> no existe o puede que se haya eliminado.</p>
+      </div>
+      <div className="flex gap-3">
+        <Link href={'/dashboard'}>
+          <Button variant={'outline'}>Regresar</Button>
+        </Link>
+        <Link href={'/'}>
+          <Button>Regresar al inicio</Button>
+        </Link>
+      </div>
     </main>
   )
 }
